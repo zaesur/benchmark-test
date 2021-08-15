500fb02ba3f457d7618c74d68b754d7930a34881
Florian Van Belleghem <fvbe@stylelabs.com>
2021-08-15 12:49:07 +0200

    Refactor fibonacci

--- src/main/java/com/example/Fibonacci.java
+++ src/main/java/com/example/Fibonacci.java
@@ -2,15 +2,6 @@ package com.example;
 
 public class Fibonacci {
     public static int fibonacci(int n) {
-        int prev = 0, curr = 1, hold;
-        if (n == 0) {
-            return prev;
-        }
-        for (int i = 2; i <= n; i++) {
-            hold = prev + curr;
-            prev = curr;
-            curr = hold;
-        }
-        return curr;
+        return n <= 1 ? n : fibonacci(n-1) + fibonacci(n-2);
     }
 }
